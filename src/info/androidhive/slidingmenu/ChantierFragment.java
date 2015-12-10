54c6e4d01475adf298b236338ab5de723a040d04
faridc76 <farid.chouakria@live.fr>
2015-12-10 10:41:54 +0100

    1iere mise à jour
    Première mise à jour

---
+++ src/info/androidhive/slidingmenu/ChantierFragment.java
@@ -0,0 +1,20 @@
+package info.androidhive.slidingmenu;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ChantierFragment extends Fragment {
+	
+	public ChantierFragment(){}
+	
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+ 
+        View rootView = inflater.inflate(R.layout.fragment_chantier, container, false);
+         
+        return rootView;
+	}
+}
---
+++ src/info/androidhive/slidingmenu/CodisFragment.java
@@ -0,0 +1,20 @@
+package info.androidhive.slidingmenu;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CodisFragment extends Fragment {
+	
+	public CodisFragment(){}
+	
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+ 
+        View rootView = inflater.inflate(R.layout.fragment_codis, container, false);
+         
+        return rootView;
+	}
+}
