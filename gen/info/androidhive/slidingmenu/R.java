2b484067e36d6c9baabe29cf035841c16986de1c
Sheyk <sheyks@live.com>
2015-12-17 06:58:25 +0100

    Page Chantier - ajout d'une liste

--- gen/info/androidhive/slidingmenu/R.java
+++ gen/info/androidhive/slidingmenu/R.java
@@ -68,20 +68,22 @@ public final class R {
         public static final int Button02=0x7f0a0009;
         public static final int EditText01=0x7f0a0008;
         public static final int EditText02=0x7f0a0001;
-        public static final int EditText03=0x7f0a0010;
-        public static final int TextView01=0x7f0a000f;
-        public static final int action_settings=0x7f0a0012;
+        public static final int EditText03=0x7f0a0011;
+        public static final int TextView01=0x7f0a0010;
+        public static final int action_settings=0x7f0a0014;
         public static final int button1=0x7f0a0003;
+        public static final int chantier_list_view=0x7f0a000d;
         public static final int checkBox1=0x7f0a0002;
         public static final int counter=0x7f0a000c;
         public static final int drawer_layout=0x7f0a0005;
         public static final int editText1=0x7f0a0000;
-        public static final int editText2=0x7f0a000e;
+        public static final int editText2=0x7f0a000f;
         public static final int frame_container=0x7f0a0006;
         public static final int icon=0x7f0a000a;
+        public static final int list_item_text=0x7f0a0013;
         public static final int list_slidermenu=0x7f0a0007;
-        public static final int spinner1=0x7f0a0011;
-        public static final int textView1=0x7f0a000d;
+        public static final int spinner1=0x7f0a0012;
+        public static final int textView1=0x7f0a000e;
         public static final int title=0x7f0a000b;
     }
     public static final class layout {
@@ -95,6 +97,7 @@ public final class R {
         public static final int fragment_contact=0x7f030007;
         public static final int fragment_devis=0x7f030008;
         public static final int fragment_plan=0x7f030009;
+        public static final int list_item=0x7f03000a;
     }
     public static final class menu {
         public static final int activity_register=0x7f090000;
--- src/info/androidhive/slidingmenu/ChantierFragment.java
+++ src/info/androidhive/slidingmenu/ChantierFragment.java
@@ -1,11 +1,21 @@
 package info.androidhive.slidingmenu;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ChantierFragment extends Fragment {
+	
+	ArrayList<String> arrayList;
+	ArrayAdapter<String> arrayAdapter;
+	ListView listView;
 	
 	public ChantierFragment(){}
 	
@@ -14,7 +24,18 @@ public class ChantierFragment extends Fragment {
             Bundle savedInstanceState) {
  
         View rootView = inflater.inflate(R.layout.fragment_chantier, container, false);
-         
+        listView = (ListView) rootView.findViewById(R.id.chantier_list_view);
+        String[] items = {"Angers","Amiens","Aix-en-Provence","Bordeaux","Caen","Dijon","Elbeuf","Fontainebleau","Grenoble","Issy-les-Moulineaux","Lille","Massy","Marseilles","Paris","Rouen","Versailles"};
+        arrayList = new ArrayList<>(Arrays.asList(items));
+        arrayAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item, R.id.list_item_text, arrayList);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        	@Override
+        	public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+        		//do stuff
+        	}
+		});
+        
         return rootView;
 	}
 }
