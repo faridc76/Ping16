2e761b62676dccedebe7b00d0a8472dd525cc331
romane <iliasse.romane@gmail.com>
2016-01-30 01:58:59 +0100

    Chat client

---
+++ src/info/androidhive/webgroupchat/other/WsConfig.java
@@ -0,0 +1,6 @@
+
+package info.androidhive.webgroupchat.other;
+
+public class WsConfig {
+	public static final String URL_WEBSOCKET = "http://85.171.45.34:8080/WebMobileGroupChatServer/chat?name=";
+}
