58afd4e37e91ccc01df8ed6f721f6e41460638b4
romane <iliasse.romane@gmail.com>
2016-02-03 17:18:00 +0100

    BDC button listeners

--- src/info/androidhive/slidingmenu/BdcFragment1.java
+++ src/info/androidhive/slidingmenu/BdcFragment1.java
@@ -4,20 +4,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import info.androidhive.gestineo.R;
 
-public class BdcFragment1 extends Fragment {
-
+public class BdcFragment1 extends Fragment implements View.OnClickListener {
+	
 	public BdcFragment1(){}
+	
+	public Button button1;
+	
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+ 
+        View rootView = inflater.inflate(R.layout.fragment_bdc_tab1, container, false);
+        button1 = (Button)rootView.findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+        return rootView;
+	}
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-
-		View rootView = inflater.inflate(R.layout.fragment_bdc_tab1, container, false);
-
-
-
-		return rootView;
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
 	}
 }
