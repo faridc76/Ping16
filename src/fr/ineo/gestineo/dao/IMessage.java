4a232dfb96897da19bf30cf9ffa27dbf50ce2a75
faridc76 <farid.chouakria@live.fr>
2016-02-04 20:18:53 +0100

    Commit messagedb

---
+++ src/fr/ineo/gestineo/dao/IMessage.java
@@ -0,0 +1,15 @@
+package fr.ineo.gestineo.dao;
+
+import java.util.List;
+
+import fr.ineo.gestineo.dto.Affaire;
+import fr.ineo.gestineo.dto.Message;
+import fr.ineo.gestineo.dto.Utilisateur;
+
+public interface IMessage {
+	
+	public boolean ajoutMessage(Message m);
+	
+	public void listeMessages(Affaire aff, List<Message> messages, Utilisateur userConnected);
+
+}
